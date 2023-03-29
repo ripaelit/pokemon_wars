a6a8fd3bb30d00fb1443b3a2f36d91cf8146d339
ripaelit <ripaelit1111@gmail.com>
2023-03-30 01:41:07 +0800

    Added transferring to level 2 func

--- Pokemon-attack/packages/hardhat/contracts/Game.sol
+++ Pokemon-attack/packages/hardhat/contracts/Game.sol
@@ -67,16 +67,44 @@ contract PokemonAttack is ERC1155LazyMint {
   }
 
   /*
+  @dev Transfer level 1 pickachu to upgrade to a level 2 
+  */
+  function safeTransferFrom(
+    address from,
+    address to,
+    uint256 id,
+    uint256 amount,
+    bytes memory data
+  ) public override isGameActive {
+    require(games[gameId].gameEndingTime > block.timestamp, "GAME ENDED");
+    require(id == 0, "This NFT is not transferrable");
+    super.safeTransferFrom(from, to, id, amount, data);
+    if (from != to && id == 0) {
+      // transferring level 1 pickachu should give the user a level 2 pickachu
+      _mint(msg.sender, 1, 1, "");
+      // emit events here
+    }
+  }
+
+  /*
   @dev verify the claim
   */
-  function verifyClaim(address _claimer, uint256 _tokenId, uint256 _quantity) public view virtual override {
+  function verifyClaim(
+    address _claimer,
+    uint256 _tokenId,
+    uint256 _quantity
+  ) public view virtual override isGameActive {
     // Your custom claim restriction logic
+    require(_tokenId == 0, "Only Level 1 pickachu can be claimed");
+    require(balanceOf[msg.sender][0] == 0, "Already got a level 1 pickachu");
+    require(balanceOf[msg.sender][1] == 0, "Already got a level 2 pickachu");
+    require(balanceOf[msg.sender][2] == 0, "Already got a level 3 pickachu");
   }
 
   /*
   @dev Allow the owner to be able to pause the contract
   */
-  function pauseGame() public {
+  function pauseGame() external {
     require(msg.sender == owner(), "NOT OWNER");
     started = false;
   }
