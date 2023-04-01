b828ad9f69bd42806799cb12465190746a2bfb1d
ripaelit <ripaelit1111@gmail.com>
2023-04-01 16:13:16 +0800

    Winners component added

---
+++ Pokemon-attack/packages/nextjs/components/WinnersDetails.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const WinnerDetails = (): JSX.Element => {
+    return(
+        <div className="bg-transparent border-[0.5px] flex justify-around md:justify-between text-white font-medium text-md w-11/12 sm:w-9/12 rounded-sm mt-6">
+                <p className="break-all w-[150px] lg:break-normal md:pl-4">0x7B4A8d0862F049E35078E49F2561630Fac079eB9</p>
+                <p>1</p>
+                <p className="mr-28">4</p>
+        </div>
+    )
+}
+
+export default WinnerDetails
---
+++ Pokemon-attack/packages/nextjs/pages/Leaderboards.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Navbar from "~~/components/Navbar";
+import Footer from "~~/components/scaffold-eth/Footer";
+import WinnerDetails from "~~/components/WinnersDetails";
+
+const Leaderboards = () => {
+    return(
+        <section className="min-h-screen">
+            <Navbar />
+            <h1
+            className="text-center py-10 text-white text-3xl font-semibold"
+            >Winners</h1>
+            <div className="flex items-center justify-center">
+            <div className="bg-gradient-to-r from-[#7B4FFF] to-[#FF8F9D] flex justify-around text-white font-medium text-md w-11/12 sm:w-9/12 rounded-sm">
+                <p className="lg:ml-10">Address</p>
+                <p className="lg:ml-32">Game</p>
+                <p className="lg:ml-32">Amount Won</p>
+            </div>
+            </div>
+            <div className="flex flex-col items-center">
+                {/* WinnerDetails */}
+                <WinnerDetails />
+            </div>
+            <Footer />
+        </section>
+    )
+}
+
+export default Leaderboards
