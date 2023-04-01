146084b44bf3ebd8ea5121d36d9222264fc5c965
ripaelit <ripaelit1111@gmail.com>
2023-04-01 15:46:32 +0800

    Added full screen on home page

--- Pokemon-attack/packages/nextjs/pages/index.tsx
+++ Pokemon-attack/packages/nextjs/pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <main>
+    <main className="min-h-screen">
       <Navbar />
       <Hero />
       <Footer />
