826abc930dbb6b28c0a10e64bb3602657204c352
ripaelit <ripaelit1111@gmail.com>
2023-04-01 15:45:53 +0800

    Finished Players page

---
+++ Pokemon-attack/packages/nextjs/components/PlayerDetails.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PlayerDetails = (): JSX.Element => {
+    return(
+        <div className="bg-transparent border-[0.5px] flex justify-around text-white font-medium text-md w-11/12 sm:w-9/12 rounded-sm mt-6">
+                <p className="break-all w-[200px] lg:break-normal">0x7B4A8d0862F049E35078E49F2561630Fac079eB9</p>
+                <p>5</p>
+        </div>
+    )
+}
+
+export default PlayerDetails
--- Pokemon-attack/packages/nextjs/pages/Players.tsx
+++ Pokemon-attack/packages/nextjs/pages/Players.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import Navbar from "~~/components/Navbar";
+import PlayerDetails from "~~/components/PlayerDetails";
+import Footer from "~~/components/scaffold-eth/Footer";
 
 const Players = () => {
     return(
-        <section>
+        <section className="min-h-screen">
             <Navbar />
             <h1
             className="text-center py-10 text-white text-3xl font-semibold"
             >Players</h1>
             <div className="flex items-center justify-center">
-            <div className="bg-gradient-to-r from-[#7B4FFF] to-[#FF8F9D] flex justify-around text-white font-medium text-md w-9/12 rounded-sm">
-                <p>Address</p>
-                <p>Score</p>
+            <div className="bg-gradient-to-r from-[#7B4FFF] to-[#FF8F9D] flex justify-around text-white font-medium text-md w-11/12 sm:w-9/12 rounded-sm">
+                <p className="lg:ml-10">Address</p>
+                <p className="lg:ml-32">Score</p>
             </div>
             {/* Component here */}
             </div>
+            <div className="flex flex-col items-center">
+            <PlayerDetails />
+            <PlayerDetails />
+            </div>
+            <Footer />
         </section>
     )
 }
