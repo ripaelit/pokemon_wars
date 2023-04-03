cad156df0f71213d05c0971082bed59f18dc0fdd
ripaelit <ripaelit1111@gmail.com>
2023-04-03 17:58:02 +0800

    Added image

--- Pokemon-attack/packages/nextjs/components/Pikachu.tsx
+++ Pokemon-attack/packages/nextjs/components/Pikachu.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import pichu from "../public/assets/Pichu.svg";
+import pikachu from "../public/assets/pikachu.svg";
 import Image from "next/image";
 import Modal from "./Modal";
 
@@ -9,7 +9,7 @@ const Pikachu = (): JSX.Element => {
       <h1 className="text-white font-semibold leading-6 text-xl mb-5">
         Burn your Pikachu to get a level 3 Raichu ⚡
       </h1>
-      <Image src={pichu} width="300" height="300" alt="Pichu" />
+      <Image src={pikachu} width="300" height="300" alt="Pichu" />
       <button
         className="bg-white text-black border-0 py-2.5 px-7 rounded-lg font-semibold leading-6 mt-5"
       >
--- Pokemon-attack/packages/nextjs/components/Raichu.tsx
+++ Pokemon-attack/packages/nextjs/components/Raichu.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import pichu from "../public/assets/Pichu.svg";
+import raichu from "../public/assets/Raichu.svg";
 import Image from "next/image";
 import Modal from "./Modal";
 
@@ -25,7 +25,7 @@ const Raichu = (): JSX.Element => {
       <h1 className="text-white font-semibold leading-6 text-xl mb-5">
         Time to attack other Pokemons 🔥
       </h1>
-      <Image src={pichu} width="300" height="300" alt="Pichu" />
+      <Image src={raichu} width="300" height="300" alt="Pichu" />
       <button
         className="bg-white text-black border-0 py-2.5 px-7 rounded-lg font-semibold leading-6 mt-5"
         onClick={handleCollect}
