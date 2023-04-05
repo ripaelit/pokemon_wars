7c03e5a063baa24e206a6fb7041e26011ce1dfdc
ripaelit <ripaelit1111@gmail.com>
2023-04-05 18:08:10 +0800

    Removed toaster

--- Pokemon-attack/packages/nextjs/pages/_app.tsx
+++ Pokemon-attack/packages/nextjs/pages/_app.tsx
@@ -3,13 +3,10 @@ import type { AppProps } from "next/app";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
 import NextNProgress from "nextjs-progressbar";
-import { Toaster } from "react-hot-toast";
 import { WagmiConfig } from "wagmi";
 import { BlockieAvatar } from "~~/components/scaffold-eth";
 import { useEthPrice } from "~~/hooks/scaffold-eth";
 import { useAppStore } from "~~/services/store/store";
-// import { wagmiClient } from "~~/services/web3/wagmiClient";
-// import { appChains } from "~~/services/web3/wagmiConnectors";
 import "~~/styles/globals.css";
 import '@rainbow-me/rainbowkit/styles.css';
 import {
@@ -21,7 +18,6 @@ import { publicProvider } from 'wagmi/providers/public';
 const { chains, provider } = configureChains(
   [polygonMumbai],
   [
-    // alchemyProvider({ apiKey: process.env.ALCHEMY_ID }),
     publicProvider()
   ]
 );
@@ -54,7 +50,6 @@ const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
         <div>
             <Component {...pageProps} />
         </div>
-        <Toaster />
       </RainbowKitProvider>
     </WagmiConfig>
   );
