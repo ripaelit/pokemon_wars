fdb081b507ea5171fd66a550b9cee10209d7a395
ripaelit <ripaelit1111@gmail.com>
2023-04-06 00:19:10 +0800

    Fetched winners successfully

--- Pokemon-attack/packages/nextjs/components/WinnersDetails.tsx
+++ Pokemon-attack/packages/nextjs/components/WinnersDetails.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 
-interface Props {
-    winner: string;
-    idx: number
-}
-
-const WinnerDetails = ({ winner, idx }: Props): JSX.Element => {
+const WinnerDetails = (props: any): JSX.Element => {
+    const { idx, winner, reward } = props
     return(
         <div className="bg-transparent border-[0.5px] flex justify-around md:justify-between text-white font-medium text-md w-11/12 sm:w-9/12 rounded-sm mt-6"
         id={idx.toString()}
         >
                 <p className="break-all w-[150px] lg:break-normal md:pl-4">{winner}</p>
-                <p>1</p>
-                <p className="mr-28">4</p>
+                <p className="mr-28">{idx}</p>
+                <p>{reward}</p>
         </div>
     )
 }
--- Pokemon-attack/packages/nextjs/pages/Leaderboards.tsx
+++ Pokemon-attack/packages/nextjs/pages/Leaderboards.tsx
@@ -1,16 +1,51 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Navbar from "~~/components/Navbar";
 import Footer from "~~/components/scaffold-eth/Footer";
 import WinnerDetails from "~~/components/WinnersDetails";
 import { Spinner } from "~~/components/Spinner";
-import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+import { useProvider, useContract } from "wagmi";
+import { CONTRACT_ADDR, ABI } from "~~/generated/hardhat_contracts";
 
 const Leaderboards = () => {
-    const { data: winners } = useScaffoldContractRead({
-      contractName: "Game_Contract",
-      functionName: "getAllWinners"
-    })
 
+  const [gamesArray, setGamesArray] = useState<any[]>([]);
+    const provider = useProvider();
+    const contract = useContract({
+        address: CONTRACT_ADDR,
+        abi: ABI,
+        signerOrProvider: provider
+    })
+    
+    const getGamesMap = async (id: number) => {
+        try {
+            const _game = await contract?.games(id);
+            return _game;
+        } catch (err: any) {
+            console.error(err.reason);
+        }
+    }
+
+    const getAllGames = async () => {
+        try {
+            const _gameId = await contract?.gameId();
+            // maybe log it here if it throws an error
+            const _gamesArray = [];
+            for(let i = 0; i < _gameId.toNumber(); i++) {
+              console.log(_gameId.toNumber(), "gameId here");
+                const gamesMap = getGamesMap(i);
+                _gamesArray.push(gamesMap);
+            }
+            const games = await Promise.all(_gamesArray);
+            console.log(games);
+            setGamesArray(games);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    useEffect(() => {
+        getAllGames();
+    }, [gamesArray])
     return(
         <section className="min-h-screen">
             <Navbar />
@@ -25,8 +60,8 @@ const Leaderboards = () => {
             </div>
             </div>
             <div className="flex flex-col items-center">
-              {winners && winners.map((winner, idx) => {
-                return <WinnerDetails winner={winner} idx={idx} />
+              {gamesArray && gamesArray.map((game, idx) => {
+                return <WinnerDetails {...game} idx={idx} />
               })}
             </div>
             <Footer />
