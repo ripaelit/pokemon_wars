9286c34dbcd6ccd9a4da1366e54fa105a4addf24
ripaelit <ripaelit1111@gmail.com>
2023-04-05 17:17:50 +0800

    Added reward section

--- Pokemon-attack/packages/nextjs/components/Score.tsx
+++ Pokemon-attack/packages/nextjs/components/Score.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAccount } from "wagmi";
 import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+import { ethers } from "ethers";
 
 const Score = () => {
     const { address: connectedAddress } = useAccount();
@@ -9,17 +10,26 @@ const Score = () => {
         functionName: "gameId"
     })
 
+    const { data: rewardAmount } = useScaffoldContractRead({
+        contractName: "Game_Contract",
+        functionName: "getContractBalance"
+    })
+
     const { data: connectedPlayerScore } = useScaffoldContractRead({
        contractName: "Game_Contract",
        functionName: "getScore",
        args: [connectedAddress] 
-    }) 
-
+    })
+    let rewardAmountInEther: string | undefined;
+    if(rewardAmount) {
+        rewardAmountInEther = ethers.utils.formatEther(rewardAmount);
+    }
     return(
         <div
         className="flex justify-between px-20 pt-10 text-white text-3xl"
         >
             <h1>Score: {connectedPlayerScore?.toString()}</h1>
+            <h1 className="animate-bounce">🎰 Jackpot : {rewardAmountInEther} Matic</h1>
             <h1>Game: {currentGame?.toString()}</h1>
         </div>
     )
