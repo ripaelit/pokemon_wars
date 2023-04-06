cdb5afdf723281cb528b7461405aa547aca9073a
ripaelit <ripaelit1111@gmail.com>
2023-04-06 23:31:07 +0800

    Fixed coloring issue

--- Pokemon-attack/packages/nextjs/components/WinnersDetails.tsx
+++ Pokemon-attack/packages/nextjs/components/WinnersDetails.tsx
@@ -14,7 +14,7 @@ const WinnerDetails = (props: any): JSX.Element => {
     const rewardInEther = reward ? ethers.utils.formatUnits(reward, "ether") : 0;
 
     return(
-        <div className="bg-transparent border-[0.5px] flex justify-around text-white font-medium text-md w-11/12 sm:w-9/12 rounded-sm">
+        <div className="bg-transparent border-[0.5px] flex my-2 justify-around text-white font-medium text-md w-11/12 sm:w-9/12 rounded-sm">
             {winners && winners.map((winner: string, index: number) => (
                 <div key={index} className="w-full flex items-center justify-around">
                         <Address address={winner} />
