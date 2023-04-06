81b41ac97e305d3b4377893c709c6386d6208e80
ripaelit <ripaelit1111@gmail.com>
2023-04-06 17:21:25 +0800

    Added bg-color to modal

--- Pokemon-attack/packages/nextjs/components/Modal.tsx
+++ Pokemon-attack/packages/nextjs/components/Modal.tsx
@@ -62,7 +62,7 @@ const Modal = ({ isOpen, onClose, onTransfer, title, description, action }: Moda
       >
         <div className="flex items-center justify-center min-h-screen px-4 text-center">
           <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
-          <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden bg-[#171923] text-left align-middle transition-all transform bg-white shadow-xl rounded-md">
+          <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden bg-gray-900 text-left align-middle transition-all transform bg-white shadow-xl rounded-md">
             <div className="absolute top-0 right-0 pt-4 pr-4">
               <button
                 className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-500"
