6564b9346bb49241d7ffa4cd2cbac9768e2ec7c3
ripaelit <ripaelit1111@gmail.com>
2023-05-12 17:56:52 +0800

    Removed unnecessary function calls

--- Pokemon-attack/packages/nextjs/components/WinnersDetails.tsx
+++ Pokemon-attack/packages/nextjs/components/WinnersDetails.tsx
@@ -4,23 +4,23 @@ import { ethers } from "ethers";
 import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
 
 const WinnerDetails = (props: any): JSX.Element => {
-    const { data: winners } = useScaffoldContractRead({
-        contractName: "Game_Contract",
-        functionName: "getAllWinners",
-    })
 
-    const { reward } = props
+    const { reward, winner } = props
     // Convert reward from wei to ether
     const rewardInEther = reward ? ethers.utils.formatUnits(reward, "ether") : 0;
 
     return(
         <div className="bg-transparent border-[0.5px] flex my-2 justify-around text-white font-medium text-md w-11/12 sm:w-9/12 rounded-sm">
-            {winners && winners.map((winner: string, index: number) => (
+            {/* {winners && winners.map((winner: string, index: number) => (
                 <div key={index} className="w-full flex items-center justify-around">
                         <Address address={winner} />
                         <p>{rewardInEther} Matic</p>
                 </div>
-            ))}
+            ))} */}
+            <div className="w-full flex items-center justify-around">
+                        <Address address={winner} />
+                        <p>{rewardInEther} Matic</p>
+            </div>
         </div>
     )
 }
